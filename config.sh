28978a47620f6d98e7446dc38766f89bed59b4fc
igwtcode <93984311+igwtcode@users.noreply.github.com>
2024-09-24 22:34:48 +0200

    add tmux and starship configs

--- config.sh
+++ config.sh
@@ -4,10 +4,30 @@ DIR=$(pwd)
 CONF_DIR=$DIR/config
 DOT_CONF_DIR=$HOME/.config
 
-ln -sfn $CONF_DIR/nvim $DOT_CONF_DIR/nvim
-ln -sfn $CONF_DIR/alacritty $DOT_CONF_DIR/alacritty
-ln -sfn $CONF_DIR/lazygit $DOT_CONF_DIR/lazygit
-ln -sfn $CONF_DIR/.gitconfig $HOME/.gitconfig
-ln -sfn $CONF_DIR/bat $DOT_CONF_DIR/bat
+dotconfig_items=(
+  "alacritty"
+  "bat"
+  "lazygit"
+  "nvim"
+  "starship"
+  "tmux"
+)
+
+for item in "${dotconfig_items[@]}"; do
+  ln -sfn $CONF_DIR/$item $DOT_CONF_DIR/$item
+done
+
+home_items=(
+  ".gitconfig"
+)
+
+for item in "${home_items[@]}"; do
+  ln -sfn $CONF_DIR/$item $HOME/$item
+done
+
+tpm_dir=$DOT_CONF_DIR/tmux/plugins/tpm
+if [ ! -d $tpm_dir ]; then
+  git clone https://github.com/tmux-plugins/tpm $tpm_dir
+fi
 
 bat cache --build
