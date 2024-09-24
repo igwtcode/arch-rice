c9713f55b401fc24d00b66c1d52e94e9ce0d04d6
igwtcode <93984311+igwtcode@users.noreply.github.com>
2024-09-24 21:36:17 +0200

    add bat config

--- config.sh
+++ config.sh
@@ -5,4 +5,8 @@ CONF_DIR=$DIR/config
 DOT_CONF_DIR=$HOME/.config
 
 ln -sfn $CONF_DIR/nvim $DOT_CONF_DIR/nvim
+
+ln -sfn $CONF_DIR/bat $DOT_CONF_DIR/bat
+bat cache --build
+
 ln -sfn $CONF_DIR/.gitconfig $HOME/.gitconfig
