639c41b1a02c71159382192a6f10a891e2bb6dd9
igwtcode <93984311+igwtcode@users.noreply.github.com>
2024-09-24 21:37:55 +0200

    add lazygit config

--- config.sh
+++ config.sh
@@ -9,4 +9,6 @@ ln -sfn $CONF_DIR/nvim $DOT_CONF_DIR/nvim
 ln -sfn $CONF_DIR/bat $DOT_CONF_DIR/bat
 bat cache --build
 
+ln -sfn $CONF_DIR/lazygit $DOT_CONF_DIR/lazygit
+
 ln -sfn $CONF_DIR/.gitconfig $HOME/.gitconfig
