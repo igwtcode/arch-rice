f61e3fa42712f2f16c2320752e4208942ba9fbbd
igwtcode <93984311+igwtcode@users.noreply.github.com>
2024-09-24 21:41:11 +0200

    add alacritty config

--- config.sh
+++ config.sh
@@ -5,10 +5,9 @@ CONF_DIR=$DIR/config
 DOT_CONF_DIR=$HOME/.config
 
 ln -sfn $CONF_DIR/nvim $DOT_CONF_DIR/nvim
-
-ln -sfn $CONF_DIR/bat $DOT_CONF_DIR/bat
-bat cache --build
-
+ln -sfn $CONF_DIR/alacritty $DOT_CONF_DIR/alacritty
 ln -sfn $CONF_DIR/lazygit $DOT_CONF_DIR/lazygit
-
 ln -sfn $CONF_DIR/.gitconfig $HOME/.gitconfig
+ln -sfn $CONF_DIR/bat $DOT_CONF_DIR/bat
+
+bat cache --build
