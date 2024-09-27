ff71206879aee4a4a98b84a3d57b42446c344f87
igwtcode <93984311+igwtcode@users.noreply.github.com>
2024-09-27 13:10:51 +0200

    fzf catppuccin theme

--- config/shellrc/common.sh
+++ config/shellrc/common.sh
@@ -33,6 +33,13 @@ export LG_CONFIG_FILE=$HOME/.config/lazygit/config.yaml
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
 
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
+
 export GOPATH=$HOME/go
 export PATH=$PATH:$HOME/.local/bin:$GOPATH/bin/bin
 export MOZ_ENABLE_WAYLAND=1
