6a15f7c52e5818df716961bbdcd54c582730668c
igwtcode <93984311+igwtcode@users.noreply.github.com>
2024-09-28 09:48:14 +0200

    modify hyprland once.sh script

--- config/hypr/scripts/once.sh
+++ config/hypr/scripts/once.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 # Kill existing instances of required services
-pkill -x xdg-desktop-portal-wlr
-pkill -x hyprpaper
-pkill -x waybar
-pkill -x hypridle
-pkill -x xdg-desktop-portal
+killall -q \
+  hyprpaper \
+  waybar \
+  hypridle \
+  dunst \
+  polkit-gnome-authentication-agent \
+  xdg-desktop-portal-wlr \
+  xdg-desktop-portal
 
 # Update environment variables for dbus
 dbus-update-activation-environment --systemd --all
 
 # Start necessary services
-hyprpaper &
 waybar &
 hypridle &
-
-# Start the polkit agent
+hyprpaper &
+dunst &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-
-# Start xdg-desktop-portal services with a slight delay
 /usr/lib/xdg-desktop-portal-wlr &
 sleep 1
 /usr/lib/xdg-desktop-portal &
